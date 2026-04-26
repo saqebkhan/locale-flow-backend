2662da9bf51ae092ad2eeb19cca699b2527e82b1
saqebkhan <saqeb.khan@thesouledstore.com>
2026-04-26 22:50:59 +0530

    fix err

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,22 +1,23 @@
 import express from 'express';
-import { createProject, getProjects, getProjectKeys, createNewKey, getProjectDetails, updateProject, inviteMember, getProjectMembers, acceptInvitation, updateMemberRole, removeMember } from '../controllers/projectController';
+import { createProject, getProjects, getProjectKeys, createNewKey, getProjectDetails, updateProject, inviteMember, getProjectMembers, acceptInvitation, updateMemberRole, removeMember, deleteApiKey, rotateKey } from '../controllers/projectController';
+import { auth } from '../middleware/auth';
 
 const router = express.Router();
 
-router.route('/').post(createProject).get(getProjects);
-router.route('/:id').get(getProjectDetails).put(updateProject);
-router.post('/:id/invite', inviteMember);
-router.get('/:id/members', getProjectMembers);
-router.get('/:id/keys', getProjectKeys);
-router.post('/:id/keys', createNewKey);
-router.delete('/:id/keys/:keyId', deleteApiKey);
-router.post('/:id/keys/:keyId/rotate', rotateKey);
+router.route('/').post(auth, createProject).get(auth, getProjects);
+router.route('/:id').get(auth, getProjectDetails).put(auth, updateProject);
+router.post('/:id/invite', auth, inviteMember);
+router.get('/:id/members', auth, getProjectMembers);
+router.get('/:id/keys', auth, getProjectKeys);
+router.post('/:id/keys', auth, createNewKey);
+router.delete('/:id/keys/:keyId', auth, deleteApiKey);
+router.post('/:id/keys/:keyId/rotate', auth, rotateKey);
 
 // Member Management
-router.put('/:id/members/:memberId', updateMemberRole);
-router.delete('/:id/members/:memberId', removeMember);
+router.put('/:id/members/:memberId', auth, updateMemberRole);
+router.delete('/:id/members/:memberId', auth, removeMember);
 
 // Invitation Handling
-router.post('/invitations/:token/accept', acceptInvitation);
+router.post('/invitations/:token/accept', auth, acceptInvitation);
 
 export default router;
