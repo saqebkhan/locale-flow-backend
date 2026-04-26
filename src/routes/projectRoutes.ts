a3460382b69bc33457f2aae8df76906fc886ed47
saqebkhan <saqeb.khan@thesouledstore.com>
2026-04-26 22:54:06 +0530

    fics beld ishu

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,23 +1,23 @@
 import express from 'express';
 import { createProject, getProjects, getProjectKeys, createNewKey, getProjectDetails, updateProject, inviteMember, getProjectMembers, acceptInvitation, updateMemberRole, removeMember, deleteApiKey, rotateKey } from '../controllers/projectController';
-import { auth } from '../middleware/auth';
+import { protect } from '../middleware/auth';
 
 const router = express.Router();
 
-router.route('/').post(auth, createProject).get(auth, getProjects);
-router.route('/:id').get(auth, getProjectDetails).put(auth, updateProject);
-router.post('/:id/invite', auth, inviteMember);
-router.get('/:id/members', auth, getProjectMembers);
-router.get('/:id/keys', auth, getProjectKeys);
-router.post('/:id/keys', auth, createNewKey);
-router.delete('/:id/keys/:keyId', auth, deleteApiKey);
-router.post('/:id/keys/:keyId/rotate', auth, rotateKey);
+router.route('/').post(protect, createProject).get(protect, getProjects);
+router.route('/:id').get(protect, getProjectDetails).put(protect, updateProject);
+router.post('/:id/invite', protect, inviteMember);
+router.get('/:id/members', protect, getProjectMembers);
+router.get('/:id/keys', protect, getProjectKeys);
+router.post('/:id/keys', protect, createNewKey);
+router.delete('/:id/keys/:keyId', protect, deleteApiKey);
+router.post('/:id/keys/:keyId/rotate', protect, rotateKey);
 
 // Member Management
-router.put('/:id/members/:memberId', auth, updateMemberRole);
-router.delete('/:id/members/:memberId', auth, removeMember);
+router.put('/:id/members/:memberId', protect, updateMemberRole);
+router.delete('/:id/members/:memberId', protect, removeMember);
 
 // Invitation Handling
-router.post('/invitations/:token/accept', auth, acceptInvitation);
+router.post('/invitations/:token/accept', protect, acceptInvitation);
 
 export default router;
